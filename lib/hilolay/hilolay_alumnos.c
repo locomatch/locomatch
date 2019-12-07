024b98980b53f6e629043879f940252c0baef52e
contimro <contimro@gmail.com>
2019-12-06 21:37:27 -0300

    Modifica hilolay lib

--- lib/hilolay/hilolay_alumnos.c
+++ lib/hilolay/hilolay_alumnos.c
@@ -1,5 +1,7 @@
-#include <hilolay_alumnos.h>
+#include <hilolay/alumnos.h>
 #include <linuse/sockets/sockets.h>
+#include <commons/collections/list.h>
+#include <commons/string.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -7,21 +9,15 @@
 
 int suse_socket;
 
-static struct hilolay_operations hiloops = {
-		.suse_create = &suse_create,
-		.suse_schedule_next = &suse_schedule_next,
-		.suse_join = &suse_join,
-		.suse_close = &suse_close,
-		.suse_wait = &suse_wait,
-		.suse_signal = &suse_signal
-};
-
 int suse_create(int tid){
 
+	char* threadid = string_itoa(tid);
+
 	t_paquete *create = crear_paquete(SUSE_CREATE);
-	agregar_a_paquete(create, &tid, sizeof(int));
+	agregar_a_paquete(create, threadid, sizeof(int));
 	enviar_paquete(create, suse_socket);
 
+	free(threadid);
 	return 0;
 }
 
@@ -32,7 +28,7 @@ int suse_schedule_next(void){
 	if(recibir_operacion(suse_socket) == SUSE_SCHEDULE_NEXT_RETURN){
 		t_list *parametros = list_create();
 		parametros = recibir_paquete(suse_socket);
-		return (int)list_get(parametros, 0);
+		return atoi((char*)list_get(parametros, 0));
 	}
 
 	return 0;
@@ -40,41 +36,62 @@ int suse_schedule_next(void){
 
 int suse_join(int tid){
 
-	t_paquete *create = crear_paquete(SUSE_JOIN);
-	agregar_a_paquete(create, &tid, sizeof(int));
-	enviar_paquete(create, suse_socket);
+	char* threadid = string_itoa(tid);
 
+	t_paquete *join = crear_paquete(SUSE_JOIN);
+	agregar_a_paquete(join, threadid, sizeof(int));
+	enviar_paquete(join, suse_socket);
+
+	free(threadid);
 	return 0;
 }
 
 int suse_close(int tid){
+	
+	char* threadid = string_itoa(tid);
 
-	t_paquete *create = crear_paquete(SUSE_CLOSE);
-	agregar_a_paquete(create, &tid, sizeof(int));
-	enviar_paquete(create, suse_socket);
+	t_paquete *close = crear_paquete(SUSE_CLOSE);
+	agregar_a_paquete(close, threadid, sizeof(int));
+	enviar_paquete(close, suse_socket);
 
+	free(threadid);
 	return 0;
 }
 
 int suse_wait(int tid, char *sem_name){
 
-	t_paquete *create = crear_paquete(SUSE_WAIT);
-	agregar_a_paquete(create, &tid, sizeof(int));
-	agregar_a_paquete(create, sem_name, strlen(sem_name)+1);
-	enviar_paquete(create, suse_socket);
+	char* threadid = string_itoa(tid);
 
+	t_paquete *wait = crear_paquete(SUSE_WAIT);
+	agregar_a_paquete(wait, threadid, sizeof(int));
+	agregar_a_paquete(wait, sem_name, strlen(sem_name)+1);
+	enviar_paquete(wait, suse_socket);
+
+	free(threadid);
 	return 0;
 }
 
 int suse_signal(int tid, char *sem_name){
 
-	t_paquete *create = crear_paquete(SUSE_SIGNAL);
-	agregar_a_paquete(create, &tid, sizeof(int));
-	agregar_a_paquete(create, sem_name, strlen(sem_name)+1);
-	enviar_paquete(create, suse_socket);
+	char* threadid = string_itoa(tid);
 
+	t_paquete *signal = crear_paquete(SUSE_SIGNAL);
+	agregar_a_paquete(signal, threadid, sizeof(int));
+	agregar_a_paquete(signal, sem_name, strlen(sem_name)+1);
+	enviar_paquete(signal, suse_socket);
+
+	free(threadid);
 	return 0;
 }
+
+static struct hilolay_operations hiloops = {
+		.suse_create = &suse_create,
+		.suse_schedule_next = &suse_schedule_next,
+		.suse_join = &suse_join,
+		.suse_close = &suse_close,
+		.suse_wait = &suse_wait,
+		.suse_signal = &suse_signal
+};
 
 void hilolay_init(void){
 
