281f2cff32009bfdc82c50e39bf1c39820d2a25f
contimro <contimro@gmail.com>
2019-12-12 19:19:09 -0300

    Add Hilolay config

--- lib/hilolay/hilolay_alumnos.c
+++ lib/hilolay/hilolay_alumnos.c
@@ -2,12 +2,17 @@
 #include <linuse/sockets/sockets.h>
 #include <commons/collections/list.h>
 #include <commons/string.h>
+#include <commons/config.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define CONFIG_PATH "/home/utnso/workspace/tp-2019-2c-Los-Borbotones/lib/hilolay/hilolay.ini"
+
 int suse_socket;
+char* suse_ip;
+char* suse_port;
 
 int suse_create(int tid){
 
@@ -31,7 +36,7 @@ int suse_schedule_next(void){
 		return atoi((char*)list_get(parametros, 0));
 	}
 
-	return 0;
+	return -1;
 }
 
 int suse_join(int tid){
@@ -47,7 +52,7 @@ int suse_join(int tid){
 }
 
 int suse_close(int tid){
-	
+
 	char* threadid = string_itoa(tid);
 
 	t_paquete *close = crear_paquete(SUSE_CLOSE);
@@ -95,9 +100,40 @@ static struct hilolay_operations hiloops = {
 
 void hilolay_init(void){
 
+	t_config *config;
+
+	if((config = config_create(CONFIG_PATH)) == NULL){
+		printf("Error al crear el config.\n");
+		exit(-1);
+	}
+
+	printf("Cargando archivo de configuracion..\n");
+
+	if(config_has_property(config, "SUSE_IP")){
+		suse_ip = string_new();
+		string_append(&suse_ip, config_get_string_value(config, "SUSE_IP"));
+	}else{
+		printf("No se encuentra SUSE_IP en el archivo de configuracion.\n");
+		config_destroy(config);
+		exit(-1);
+	}
+
+	if(config_has_property(config, "SUSE_PORT")){
+		suse_port = string_new();
+		string_append(&suse_port, config_get_string_value(config, "SUSE_PORT"));
+	}else{
+		printf("No se encuentra SUSE_PORT en el archivo de configuracion.\n");
+		config_destroy(config);
+		exit(-1);
+	}
+
+	config_destroy(config);
+
+	printf("Se ha cargado correctamente el archivo de configuracion.\n");
+
 	printf("Conectandose a SUSE..\n");
 
-	suse_socket = crear_conexion_con_servidor("127.0.0.1",	"5003");
+	suse_socket = crear_conexion_con_servidor(suse_ip, suse_port);
 
 	printf("Conectado con SUSE\n");
 
